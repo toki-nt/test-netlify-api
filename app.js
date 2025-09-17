3dc5376c9b0a8e63e4c22dace08c0b677e88ce83
Tokinantenaina <tokinantenainaraboba@gmail.com>
2025-09-17 17:21:12 +0300

    suppr de app.exterieur

--- app.js
+++
@@ -1,9 +0,0 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("say hello from exterieur!!!");
-});
-
-module.exports.handler = serverless(app);
