1690d4ca1ebdae55601425cd0e2b74e4af77c8b7
Tokinantenaina <tokinantenainaraboba@gmail.com>
2025-09-17 16:45:18 +0300

    edit app exterieur

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const serverless = require('serverless-http');
-const express = require('express');
+const serverless = require("serverless-http");
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("say hello !!!")
+app.get("/", (req, res) => {
+  res.send("say hello from exterieur!!!");
 });
 
 module.exports.handler = serverless(app);
